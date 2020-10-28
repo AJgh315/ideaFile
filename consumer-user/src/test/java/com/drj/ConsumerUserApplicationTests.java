c16dff73e3e5a7a5ae083c9822099808b815682c
Ajgh315 <869284232@qq.com>
2020-10-28 10:36:42 +0800

    第一次提交

---
+++ consumer-user/src/test/java/com/drj/ConsumerUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ provider-ticket/src/main/java/com/drj/service/TicketService.java
@@ -0,0 +1,12 @@
+package com.drj.service;
+
+import org.springframework.stereotype.Service;
+
+@Service("ticketService")
+public class TicketService {
+
+    public String getTicket(){
+
+        return "come on";
+    }
+}
